638f9e344b2d8b3ba1549a46b1216bf0aa2efee6
Zane DuFour <zane.dufour@gmail.com>
2020-12-28 20:38:58 -0500

    Add install steps to tasks.py

--- tasks.py
+++ tasks.py
@@ -1,6 +1,11 @@
 from invoke import task
+import getpass
 
 @task()
 def build(c):
     """Build the resume"""
+    packages = ["texlive-latex-extra", "texlive-xetex"]
+    c.config.sudo.password = getpass.getpass("password: ")
+    for package in packages:
+        c.sudo("apt install texlive-latex-extra")
     c.run("xelatex -output-directory docs zane_resume.tex")
